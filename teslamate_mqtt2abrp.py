14aa917e4406d06537be914391ac478f017d257e
Julien <fetzu@users.noreply.github.com>
2022-04-09 00:16:54 +0200

    fix: MQTT Error Handling
    - Fixed issue in MQTT error handling
    
    Note to self: get better glasses!

--- teslamate_mqtt2abrp.py
+++ teslamate_mqtt2abrp.py
@@ -111,12 +111,12 @@ client.connect(MQTTSERVER)
 
 def on_connect(client, userdata, flags, rc):  # The callback for when the client connects to the broker
     # MQTT Error handling
-    if rc = 0: print("Connected with result code {0}. Connection with MQTT server established.".format(str(rc)))
-    elif rc = 1: sys.exit("Connection to MQTT server refused: invalid protocol version.")
-    elif rc = 2: sys.exit("Connection to MQTT server refused: invalid client identifier.")
-    elif rc = 3: sys.exit("Connection to MQTT server refused: server unavailable.")
-    elif rc = 4: sys.exit("Connection to MQTT server refused: bad username or password. Check your credentials.")
-    elif rc = 5: sys.exit("Connection to MQTT server refused: not authorised. Provide username and password as needed.")
+    if rc == 0: print("Connected with result code {0}. Connection with MQTT server established.".format(str(rc)))
+    elif rc == 1: sys.exit("Connection to MQTT server refused: invalid protocol version.")
+    elif rc == 2: sys.exit("Connection to MQTT server refused: invalid client identifier.")
+    elif rc == 3: sys.exit("Connection to MQTT server refused: server unavailable.")
+    elif rc == 4: sys.exit("Connection to MQTT server refused: bad username or password. Check your credentials.")
+    elif rc == 5: sys.exit("Connection to MQTT server refused: not authorised. Provide username and password as needed.")
     elif rc >= 6 and rc <= 255: sys.exit("Connection to MQTT server refused: unknown reason. Seems like you are really unlucky today :(.")
     client.subscribe(f"teslamate/cars/{CARNUMBER}/#")
 
